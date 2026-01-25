3c544c83f871ae867790bc9b51f96d8830669c79
Salaheddine Mourid <mouridsalah444@gmail.com>
2026-01-25 23:42:51 +0100

    Add the confirme booking files

---
+++ app/confirmeBooking/[confirmeBooking]/page.tsx
@@ -0,0 +1,11 @@
+
+import FlightsNav from "../../flies/[flies]/Containers/nav"
+
+export default function BookingConfirmation(){
+
+    return(
+        <div className="flex">
+            <FlightsNav/>
+        </div>
+    )
+}
