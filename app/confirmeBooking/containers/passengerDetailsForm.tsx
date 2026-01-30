b607fc1c9302a1dc1ad494091c4ef633e4ef0c85
Salaheddine Mourid <mouridsalah444@gmail.com>
2026-01-30 22:49:18 +0100

    fixing the passengers details form tsx ,p/2

--- app/confirmeBooking/containers/passengerDetailsForm.tsx
+++ app/confirmeBooking/containers/passengerDetailsForm.tsx
@@ -5,7 +5,7 @@ export default function PassengerForm(){
 
     return(
         <div className="bg-white p-9 border-l border-l-gray-300 h-screen w-full">
-            <form className="w-full" onClick={(e)=> e.preventDefault()}>
+            <form className="w-full" onSubmit={(e)=> e.preventDefault()}>
                 <div className="space-y-15">
                     <h1 className="flex justify-center text-xl">Adult Passenger</h1>
                     <div className="space-y-5">
@@ -16,47 +16,47 @@ export default function PassengerForm(){
                                 <h4 className="cursor-pointer">*</h4>
                             </div>
                         </div>
-                        <div className="space-y2">
+                        <div className="space-y-2">
                             <h2 className="text-lg font-medium">Gender</h2>
-                            <div className="flex">
-                                <div>
-                                    <label>Male</label>
-                                    <input type="radio" />
+                            <div className="flex  space-x-2.5">
+                                <div className="flex items-center border border-gray-300 rounded-xl py-4 px-5 space-x-2.5">
+                                    <label htmlFor="male">Male</label>
+                                        <input className="accent-red-900"  type="radio" id="male" value="male" name="class"/>                    
                                 </div>
-                                <div>
-                                    <label>Female</label>
-                                    <input type="radio" />
+                                <div className="flex items-center border border-gray-300 rounded-xl py-4 px-5 space-x-2.5">
+                                    <label htmlFor="female">Female</label>
+                                    <input className="accent-red-900" type="radio" id="female" value="female" name="class"/>
                                 </div>
                             </div>
                         </div>
-                        <div>
-                            <h2>First name/middle name on passport</h2>
-                            <input type="text"/>
+                        <div className="space-y-2">
+                            <h2 className="text-lg font-medium">First name/middle name on passport</h2>
+                            <input className="flex justify-between items-center border border-gray-300 rounded-xl p-4 w-full" type="text"/>
                         </div>
-                        <div>
-                            <h2>Last Name</h2>
-                            <input type="text"/>
+                        <div className="space-y-2">
+                            <h2 className="text-lg font-medium">Last Name</h2>
+                            <input className="flex justify-between items-center border border-gray-300 rounded-xl p-4 w-full" type="text"/>
                         </div>
-                        <div>
-                            <h2>Date of birth</h2>
-                            <div>
-                                <div>
+                        <div className="space-y-2">
+                            <h2 className="text-lg font-medium">Date of birth</h2>
+                            <div className="flex w-full space-x-4">
+                                <div className="flex justify-between items-center border border-gray-300 rounded-xl p-4 w-1/3 space-x-2.5">
                                     <h4>11</h4>
                                     <h5>*</h5>
                                 </div>
-                                <div>
+                                <div className="flex justify-between items-center border border-gray-300 rounded-xl p-4 w-1/3 space-x-2.5">
                                     <h4>Feb</h4>
                                     <h5>*</h5>
                                 </div>
-                                <div>
+                                <div className="flex justify-between items-center border border-gray-300 rounded-xl p-4 w-1/3 space-x-2.5">
                                     <h4>2002</h4>
                                     <h5>*</h5>
                                 </div>
                             </div>
                         </div>
-                        <div>
-                            <h2>Nationality</h2>
-                            <div>
+                        <div className="space-y-2">
+                            <h2 className="text-lg font-medium">Nationality</h2>
+                            <div className="flex justify-between items-center border border-gray-300 rounded-xl py-4 px-5 space-x-2.5">
                                 <h3>Morocco</h3>
                                 <h4>*</h4>
                             </div>
@@ -64,8 +64,8 @@ export default function PassengerForm(){
                         <div></div>
                     </div>
 
-                    <div>
-                        <button>Save and Continue</button>
+                    <div >
+                        <button className="bg-red-900 border-2 border-red-900 rounded-full w-full px-8 py-4  font-bold text-md text-amber-50 cursor-pointer">Save and Continue</button>
                     </div>
                 </div>
             </form>
