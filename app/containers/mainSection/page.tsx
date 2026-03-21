0b8639df6dcf2aa12dbf25f243192bbcf4507878
Salaheddine Mourid <mouridsalah444@gmail.com>
2026-03-21 19:58:30 +0000

    add travelExperience section

--- app/containers/mainSection/page.tsx
+++ app/containers/mainSection/page.tsx
@@ -1,10 +1,12 @@
 
 import VoyageSuggetions from "./voyageSuggestions"
+import travelExp from "./travelExperience"
 
 export default function MainSection(){
 
     return(
         <div>
+            <travelExp/>
             <VoyageSuggetions/>
         </div>
     )
