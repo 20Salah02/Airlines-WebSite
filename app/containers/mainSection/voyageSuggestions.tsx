818ed0b0ebf099f74f930dc06044e1ef01d9ba90
Salaheddine Mourid <mouridsalah444@gmail.com>
2026-04-09 00:15:54 +0100

    fix : voyagesuggestions first part responsive inphones

--- app/containers/mainSection/voyageSuggestions.tsx
+++ app/containers/mainSection/voyageSuggestions.tsx
@@ -151,11 +151,11 @@ export default function VoyageSuggetions(){
 
 
     return(
-        <div className=" bg-zinc-100">
+        <div className="relative bg-zinc-100">
             <div>
                 <h2 className="text-4xl font-light text-gray-600">Find great fears</h2>
             </div>
-            <div className="flex justify-between mt-7 items-start">
+            <div className="flex lg:flex-row flex-col justify-between items-start mt-7 lg:space-y-0 space-y-5">
                 <div ref={fromRef} className="flex space-x-2">
                     <label htmlFor="from">From</label>
                     <HandleDestination
@@ -175,20 +175,22 @@ export default function VoyageSuggetions(){
                         <svg className="text-zinc-600" xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24"><g fill="none" stroke="currentColor" strokeWidth={1.5}><path strokeLinecap="round" d="M4 22h16"></path><path d="m13.888 3.663l.742-.742a3.146 3.146 0 1 1 4.449 4.45l-.742.74m-4.449-4.448s.093 1.576 1.483 2.966s2.966 1.483 2.966 1.483m-4.449-4.45L7.071 10.48c-.462.462-.693.692-.891.947a5.2 5.2 0 0 0-.599.969c-.139.291-.242.601-.449 1.22l-.875 2.626m14.08-8.13l-6.817 6.817c-.462.462-.692.692-.947.891q-.451.352-.969.599c-.291.139-.601.242-1.22.448l-2.626.876m0 0l-.641.213a.848.848 0 0 1-1.073-1.073l.213-.641m1.501 1.5l-1.5-1.5"></path></g></svg>
                     </h4>
                 </div>
-                <div className="flex space-x-4">
-                    <button 
-                        className={`bg-white px-8 rounded-3xl ${tripType === "round-trip" ? "border-black" : "border-gray-300"} border   cursor-pointer`}
-                        onClick={() => setTripType("round-trip")}
-                    >
-                            Return
-                    </button>
-                    <button 
-                        className={`bg-white px-6 rounded-3xl border ${tripType === "one-way" ? "border-black" : "border-gray-300"} cursor-pointer`}
-                        onClick={() => setTripType("one-way")}
-                    >
-                            One way
-                    </button>
-                    <div ref={dropdownRef}  className="relative bg-white rounded-md border border-gray-300 w-50 cursor-pointer">
+                <div className="flex lg:flex-row flex-col-reverse lg:space-x-4 w-full">
+                    <div className="flex space-x-4 ">
+                        <button 
+                            className={`bg-white px-8 py-2 rounded-3xl ${tripType === "round-trip" ? "border-black" : "border-gray-300"} border   cursor-pointer`}
+                            onClick={() => setTripType("round-trip")}
+                        >
+                                Return
+                        </button>
+                        <button 
+                            className={`bg-white px-6 rounded-3xl border ${tripType === "one-way" ? "border-black" : "border-gray-300"} cursor-pointer`}
+                            onClick={() => setTripType("one-way")}
+                        >
+                                One way
+                        </button>
+                    </div>
+                    <div ref={dropdownRef}  className="relative bg-white rounded-md border border-gray-300 lg:w-50 w-full mb-6 cursor-pointer">
                         <div 
                             className="flex justify-between  items-center px-4"
                             onClick={() => setOpenClassType(prev => !prev)}
