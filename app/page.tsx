c1a78aacc7be8803ae404bf093993154e0588209
Salaheddine Mourid <mouridsalah444@gmail.com>
2026-04-28 22:09:25 +0100

    feat : footer part 1

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 
 import Myheader from "./containers/header";
 import MainSection from "./containers/mainSection/page";
+import Footer from "./containers/footer";
 
 export default function Home() {
   return (
     <div className="bg-zinc-100 ">
       <Myheader/>
       <MainSection/>
+      <Footer/>
     </div>
   );
 } 
